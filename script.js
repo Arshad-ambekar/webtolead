9c536d6dfa7efd882a036fee5d817d7ae7159ece
Arshad <lwcproject1@gmail.com>
2024-10-02 22:57:57 +0530

    captcha condition added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+let captchaValue = false;
 function handleSubmit(event) {
   // add method what happpen after submission
+  if (captchaValue) {
+    alert("Success Your are not robot");
+    //add additional code logic for custom field like ankit for date feild to take user input and displayed in output
+  } else {
+    alert("Please Check the captcha before submitting the form");
+    event.preventDefault(); //preventing from default event to happen or submitting the form
+  }
 }
+
+//THIS FUCNTION IS GIVEN BY GOOGLE FOR USING THE GOOGLE CATPCAH IS SALEFORCE
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
   if (response == null || response.value.trim() == "") {
@@ -13,3 +23,9 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+//THIS FUCNTION IS FOR VALIDATING ALL LEAD FEILD AND CAPTCHA ARE FILLED
+function CaptchaChecked() {
+  //if captcha checkbox is checked then only submitting the form else not
+  captchaValue = true;
+}
